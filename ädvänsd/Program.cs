e03b90cca53c056076b9b00d63778dfa3f4cdd73
ReisMiner <r1miner@outlook.com>
2021-11-22 09:38:03 +0100

    niu ädvänsed schtaff

---
+++ ädvänsd/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace ädvänsd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mainThread = new Thread(DoMainThreadStuff);
+            Thread wait4Close = new Thread(ExitStuff);
+            mainThread.Start();
+            wait4Close.Start();
+
+            while (true)
+            {
+                Console.WriteLine("This is in main program");
+            }
+        }
+
+        static void DoMainThreadStuff()
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello from se sred");
+            }
+        }
+        static void ExitStuff()
+        {
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+}
